47c58cd4c5cfed06c969d02dc82d43ff274a9325
tcuk8 <cuk.tomislav@ymail.com>
2013-11-24 17:56:58 +0100

    mapa zumirana na vukovar igranje sa markerima

--- VuBusStop/gen/com/algebra/vubusstop/R.java
+++ VuBusStop/gen/com/algebra/vubusstop/R.java
@@ -206,6 +206,10 @@ containing a value of this type.
         public static final int ic_plusone_small_off_client=0x7f02001a;
         public static final int ic_plusone_standard_off_client=0x7f02001b;
         public static final int ic_plusone_tall_off_client=0x7f02001c;
+        public static final int marker100px=0x7f02001d;
+        public static final int marker_crni80px=0x7f02001e;
+        public static final int marker_crveni80px=0x7f02001f;
+        public static final int marker_plavi80px=0x7f020020;
     }
     public static final class id {
         public static final int action_settings=0x7f050006;
